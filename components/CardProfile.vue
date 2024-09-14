49e54f36005bb35d64e1b51b3d25fa42124450d5
Dewa Putra Hernanda <dewananda089@gmail.com>
2024-09-15 01:28:07 +0700

    add cardProfile component

--- components/CardProfile.vue
+++ components/CardProfile.vue
@@ -1,16 +1,11 @@
 <template>
   <div
-    class="flex flex-col justify-center items-center bg-[#483E46] rounded-2xl basis-1/3 h-[560px]"
+    class="hidden lg:flex flex-col justify-center items-center bg-[#483E46] rounded-2xl basis-1/3 h-[560px]"
   >
-    <div class="">
+    <div class="w-full">
       <figure
-        class="bg-[#7E7A88] pt-4"
-        style="
-          border-top-right-radius: 60px;
-          border-top-left-radius: 120px;
-          border-bottom-left-radius: 46px;
-          border-bottom-right-radius: 80px;
-        "
+        class="bg-[#7E7A88] pt-4 w-fit mx-auto"
+        style="border-radius: 35px"
       >
         <img
           src="../assets/images/profile.png"
@@ -18,43 +13,72 @@
           class="mx-auto"
           style="
             border-bottom-left-radius: 35px;
-            border-bottom-right-radius: 59px;
+            border-bottom-right-radius: 35px;
           "
         />
       </figure>
-      <h1 class="text-[#C7CD83] font-bold text-2xl text-center">
-        Dewa <br /><span>Putra Hernanda</span>
-      </h1>
+      <div class="text-[#C7CD83] font-bold text-2xl text-center mb-1">
+        <h1 class="-mb-2">Dewa</h1>
+        <h2>Putra Hernanda</h2>
+      </div>
       <p class="text-center bg-[#ADAEC0] w-fit px-3 rounded-xl mx-auto">
         Programmer
       </p>
     </div>
+
     <div class="border-t-2 w-10/12 my-2"></div>
+
     <div class="w-full px-4">
       <Contac
-        title="Email"
-        detail="dewananda124@gmail.com"
-        icon="material-symbols:mail-outline-rounded"
-      />
-      <Contac
-        title="Phone"
-        detail="081326805627"
-        icon="material-symbols:call-outline-rounded"
-      />
-      <Contac title="Birthday" detail="Feb 24. 2002" icon="uil:calender" />
-      <Contac
-        title="Location"
-        detail="Sleman, Yogyakarta, Indonesia"
-        icon="streamline:travel-map-navigation-map-maps-gps"
+        v-for="contact in listContact"
+        :title="contact.title"
+        :detail="contact.detail"
+        :icon="contact.icon"
       />
     </div>
+
     <div class="flex gap-2">
-      <Icon name="ci:linkedin" class="text-3xl mt-1" style="color: #adaec0" />
-      <Icon
-        name="tabler:brand-github"
-        class="text-3xl mt-1 font-bold"
-        style="color: #adaec0"
-      />
+      <NuxtLink to="https://www.linkedin.com/in/dewa-putra-h/" target="_blank">
+        <Icon name="ci:linkedin" class="text-3xl mt-1" style="color: #adaec0" />
+      </NuxtLink>
+      <NuxtLink to="https://github.com/dewa-nanda" target="_blank">
+        <Icon
+          name="tabler:brand-github"
+          class="text-3xl mt-1 font-bold"
+          style="color: #adaec0"
+        />
+      </NuxtLink>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+  interface contact {
+    title: string
+    detail: string
+    icon: string
+  }
+
+  const listContact: contact[] = [
+    {
+      title: 'Email',
+      detail: 'dewananda124@gmail.com',
+      icon: 'material-symbols:mail-outline-rounded',
+    },
+    {
+      title: 'Phone',
+      detail: '081326805627',
+      icon: 'material-symbols:call-outline-rounded',
+    },
+    {
+      title: 'Birthday',
+      detail: 'Feb 24. 2002',
+      icon: 'uil:calender',
+    },
+    {
+      title: 'Location',
+      detail: 'Sleman, Yogyakarta, Indonesia',
+      icon: 'streamline:travel-map-navigation-map-maps-gps',
+    },
+  ]
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center gap-10 py-5 w-1/2 mx-auto">
+  <div class="flex justify-center gap-10 py-5 w-10/12 xl:w-1/2 mx-auto">
     <CardProfile />
 
     <div class="basis-full">
