1d2c29e88a54bc07cf89227aaa10fe07daddf458
Dewa Putra Hernanda <dewananda089@gmail.com>
2024-09-15 03:42:13 +0700

    add content

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,30 @@
     <h1 class="text-3xl mb-3 font-bold text-[#C7CD83]">
       <span class="border-b-2 border-[#C7CD83]">About</span> Me
     </h1>
-    <p class="text-[#ADAEC0]">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-      excepturi optio ex deleniti ea quibusdam sint veniam a quae, debitis enim
-      quaerat quos porro fuga temporibus, assumenda ducimus eius magnam nulla
-      iste esse eveniet. Dolore odit consequuntur itaque quasi illo.
+    <p class="text-[#ADAEC0] text-justify">
+      I'm a passionate <strong>Junior Front-End Web Developer</strong> with a
+      strong foundation in <strong>JavaScript</strong>. I'm constantly seeking
+      out new technologies and frameworks to expand my skill set and tackle
+      complex challenges. My ability to <strong>learn quickly</strong> and
+      <strong>adapt to new environments</strong> allows me to deliver
+      high-quality solutions that meet the needs of my clients.
     </p>
+    <ul class="text-[#ADAEC0] mt-1">
+      <li>
+        🔭 I’m currently working on <strong>geek garden software house.</strong>
+      </li>
+      <li>
+        🌱 Learning more about <strong>Vue.js</strong>, and
+        <strong>React</strong>
+      </li>
+      <li>
+        💬 Ask me about <strong>Front-End Web Development</strong>,
+        <strong>JavaScript</strong>, or anything <strong>tech-related</strong>.
+      </li>
+      <li>
+        ⚡ Fun fact: I enjoy music and exploring new places when I'm not coding.
+      </li>
+    </ul>
   </section>
   <section class="mx-6">
     <h1 class="text-3xl mb-3 font-bold text-[#C7CD83]">
--- pages/project.vue
+++ pages/project.vue
@@ -1,12 +1,19 @@
 <template>
   <section class="mx-6 mb-3">
     <h1 class="text-3xl mb-3 font-bold text-[#C7CD83]">Project</h1>
-    <p class="text-[#ADAEC0]">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-      excepturi optio ex deleniti ea quibusdam sint veniam a quae, debitis enim
-      quaerat quos porro fuga temporibus, assumenda ducimus eius magnam nulla
-      iste esse eveniet. Dolore odit consequuntur itaque quasi illo.
-    </p>
+    <div class="flex gap-2 text-[#ADAEC0] my-3">
+      <p class="hover:text-[#C7CD83]">All</p>
+      <p class="hover:text-[#C7CD83]">React</p>
+      <p class="hover:text-[#C7CD83]">Vue</p>
+    </div>
+
+    <div class="w-full grid grid-cols-4 gap-2">
+      <div class="h-[100px]">
+        <!-- <img src="#"> -->
+        <div class="bg-[#ADAEC0] h-full rounded-3xl"></div>
+        <h2 class="text-center text-[#FFFFFF] font-bold">Title</h2>
+      </div>
+    </div>
   </section>
 </template>
 
