03db1d6c4a487bf0ba0781c9098eee70a9aa7619
Dewa Putra Hernanda <dewananda089@gmail.com>
2024-09-15 02:55:49 +0700

    update pages resume

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
     <div class="basis-full bg-[#483E46] rounded-2xl">
       <header class="w-full flex justify-end rounded-2xl">
         <nav
-          class="w-fit flex justify-end bg-[#7E7A88] p-3 gap-2 font-bold text-[#483E46] rounded-se-2xl rounded-es-2xl"
+          class="w-fit flex justify-end bg-[#7E7A88] p-3 gap-2 font-bold rounded-se-2xl rounded-es-2xl text-[#483E46]"
         >
           <NuxtLink
             v-for="link in listLink"
@@ -15,7 +15,7 @@
           >
         </nav>
       </header>
-      
+
       <main>
         <slot />
       </main>
@@ -44,3 +44,9 @@
     },
   ]
 </script>
+
+<style scoped>
+  .router-link-active {
+    color: #31282d;
+  }
+</style>
--- pages/resume.vue
+++ pages/resume.vue
@@ -1,12 +1,69 @@
 <template>
   <section class="mx-6 mb-3">
-    <h1 class="text-3xl mb-3 font-bold text-[#C7CD83]">Resume</h1>
-    <p class="text-[#ADAEC0]">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-      excepturi optio ex deleniti ea quibusdam sint veniam a quae, debitis enim
-      quaerat quos porro fuga temporibus, assumenda ducimus eius magnam nulla
-      iste esse eveniet. Dolore odit consequuntur itaque quasi illo.
-    </p>
+    <div class="flex gap-2 text-[#C7CD83]">
+      <h1 class="text-3xl mb-3 font-bold">Resume</h1>
+      <NuxtLink
+        to="https://drive.google.com/file/d/1A9mZ-R4GAVkOHtIPoKabKgAw045aExFZ/view?usp=sharing"
+        target="_blank"
+      >
+        <Icon
+          name="flowbite:arrow-down-to-bracket-solid"
+          class="text-3xl mt-1 hover:text-[#adaec0]"
+        />
+      </NuxtLink>
+    </div>
+
+    <div>
+      <div class="flex items-center gap-2 text-3xl text-[#adaec0]">
+        <Icon name="flowbite:book-outline" />
+        <h2 class="font-bold">Education</h2>
+      </div>
+      <div>
+        <div class="text-[#adaec0] flex mx-2 gap-3">
+          <Icon
+            name="streamline:interface-arrows-right-circle-2-arrow-keyboard-circle-button-right"
+            class="text-[2.4rem]"
+          />
+          <div>
+            <h3 class="text-xl font-bold -mb-1">S1 Informatika</h3>
+            <p class="font-semibold">2020 - 2024</p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam ex totam, dolores possimus dolor veniam! Dicta culpa
+              rem soluta doloremque quisquam, minima nihil tenetur accusantium
+              quibusdam unde velit voluptatem ipsum expedita ab corrupti nemo
+              aliquid aspernatur nesciunt hic inventore ea?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <div class="flex items-center gap-2 text-3xl text-[#adaec0]">
+        <Icon name="solar:programming-broken" />
+        <h2 class="font-bold">Experiance</h2>
+      </div>
+      <div>
+        <div class="text-[#adaec0] flex mx-2 gap-3">
+          <Icon
+            name="streamline:interface-arrows-right-circle-2-arrow-keyboard-circle-button-right"
+            class="text-[2.4rem]"
+          />
+          <div>
+            <h3 class="text-xl font-bold -mb-1">Lazismu - Web Developer</h3>
+            <p class="font-semibold">2020 - 2024</p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam ex totam, dolores possimus dolor veniam! Dicta culpa
+              rem soluta doloremque quisquam, minima nihil tenetur accusantium
+              quibusdam unde velit voluptatem ipsum expedita ab corrupti nemo
+              aliquid aspernatur nesciunt hic inventore ea?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
