03b30af224d6be6bab19497a72da2d7295999e04
Dewa Putra Hernanda <dewananda089@gmail.com>
2024-09-15 02:16:38 +0700

    add new pages

---
+++ pages/resume.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="mx-6 mb-3">
+    <h1 class="text-3xl mb-3 font-bold text-[#C7CD83]">Resume</h1>
+    <p class="text-[#ADAEC0]">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+      excepturi optio ex deleniti ea quibusdam sint veniam a quae, debitis enim
+      quaerat quos porro fuga temporibus, assumenda ducimus eius magnam nulla
+      iste esse eveniet. Dolore odit consequuntur itaque quasi illo.
+    </p>
+  </section>
+</template>
+
+<script lang="ts" setup>
+  useSeoMeta({
+    title: 'KETweb - Resume',
+  })
+</script>
